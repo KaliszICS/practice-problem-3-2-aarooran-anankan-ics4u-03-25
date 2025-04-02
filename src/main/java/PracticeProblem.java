5f39dedae6364ea6d2daccb69170d7c02631d7aa
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-04-02 23:25:37 +0000

    Final Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,26 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+	public static int towerOfHanoi(int num) {
+		
+		int ber = num;
+		if (ber < 3) {
+			return -1;
+		}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+		if (num == 0) {
+			return 0;
+		}
+		if (num == 1) {
+			return 1;
+		}
+		if (num == 2) {
+			return 3;
+		}
+		if (num == 3) {
+			return 7;
+		}
 
-	public static void q3() {
-		//Write question 3 code here
+		return (towerOfHanoi(num-1)+1) + towerOfHanoi(num-1);
 	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
